0a0850758a9a7be7f61184f73620cdd4719cf1a4
Michael Yagudaev <michaelyagudaev@gmail.com>
2018-04-19 23:21:10 -0400

    finish writing system tests

--- spec/system/core_spec.rb
+++ spec/system/core_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 describe 'Core Features', js: true do
-  before { visit '/' }
+
 
   describe 'List posts' do
     let!(:posts) { [create(:post, title: 'post 1'), create(:post, title: 'post 2')]}
+    before { visit '/' }
 
     it 'should show all the post' do
       expect(page).to have_content('post 1')
@@ -13,6 +14,8 @@ describe 'Core Features', js: true do
   end
 
   describe 'Create a post' do
+    before { visit '/' }
+
     it 'should let me create a post' do
       fill_in 'title', with: 'post 3'
       fill_in 'content', with: 'Content goes here'
@@ -22,12 +25,24 @@ describe 'Core Features', js: true do
   end
 
   describe 'Update a post' do
+    let!(:post) { create(:post, title: 'old title', content: 'content') }
+    before { visit '/' }
+
     it 'should let me edit the post' do
-      # click_on 'edit'
-      # fill_in
+      find('a', text: 'Edit').click
+      fill_in 'title', currently_with: 'old title', with: 'new title'
+      find(:xpath, "(//button[text()='Save'])[2]").click
+      expect(page).to have_content('new title')
     end
   end
 
   describe 'Delete a post' do
+    let!(:post) { create(:post, title: 'Cannot touch this') }
+    before { visit '/' }
+
+    it 'should delete the post' do
+      find('a', text: 'Delete').click
+      expect(page).not_to have_content('Cannout touch this')
+    end
   end
 end
